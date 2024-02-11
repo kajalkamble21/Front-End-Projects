eac3ce232928b92a46cd334f4026ea3a7fab4f98
kajalkamble21 <kamblekajal604@gmail.com>
2024-02-11 12:56:43 +0530

    add files

---
+++ School Hompage Using HTML,CSS,Bootstrap/js/custom.js
@@ -0,0 +1,19 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+getYear();
+// overlay menu
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
+}
+
+
+// lightbox gallery
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
